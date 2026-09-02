5b66ad238793f3885e33e874cc6cb6c8d9c44389
fabriciopriscilapbns-spec <fabriciopriscilapbns@gmail.com>
2026-09-02 15:26:40 -0300

    Enhance service worker with caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
-self.addEventListener('install', () => {
-self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('usiminas-store').then((cache) => {
+      return cache.addAll(['./index.html']);
+    })
+  );
 });
-self.addEventListener('activate', () => {
-console.log('Service Worker ativo');
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
